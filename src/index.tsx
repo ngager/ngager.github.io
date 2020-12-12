e3e3a3da0201e7a34ea775fa49f20a7fa744b102
Nicole Gager <ngager@emersonecologics.com>
2020-12-11 22:49:12 -0500

    add console

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import App from './components/app/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+console.log('Your process.env.PUBLIC_URL', process.env.PUBLIC_URL);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
