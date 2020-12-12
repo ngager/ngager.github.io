0e5ed1f36f13ea172166b85de46f50b753130b9a
Nicole Gager <ngager@emersonecologics.com>
2020-12-11 22:15:46 -0500

    remove cache

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app';
 import reportWebVitals from './reportWebVitals';
+import App from './components/app/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
