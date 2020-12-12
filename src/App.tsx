f21fa22c18283bbc823c7f214dd65f48c622280d
Nicole Gager <ngager@emersonecologics.com>
2020-12-11 21:39:30 -0500

    add react project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>TEST</div>;
+  }
+}
+
+export default App;
